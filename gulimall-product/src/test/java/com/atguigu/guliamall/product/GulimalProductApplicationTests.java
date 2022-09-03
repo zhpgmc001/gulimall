d2247a001e2af3b867c11a1c4aa4395ba368274c
zhpgmc001 <530405355@qq.com>
2022-09-04 00:38:46 +0800

    项目初提交

---
+++ gulimall-product/src/test/java/com/atguigu/guliamall/product/GulimalProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.guliamall.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimalProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
